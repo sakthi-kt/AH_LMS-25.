907a5b783f2399552015762c372532d3482e1406
sakthi-kt <sakthiveljeevaa@gmail.com>
2022-08-26 19:04:09 +0530

    update

--- app/src/main/java/com/example/ah_lms/MainActivity.kt
+++ app/src/main/java/com/example/ah_lms/MainActivity.kt
@@ -4,19 +4,23 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val email :EditText = findViewById(R.id.emailEt)
+        val password:EditText = findViewById(R.id.passwordEt)
+        val login:Button = findViewById(R.id.login)
 
 
-           signin.setOnClickListener {
+        login.setOnClickListener {
             val intent = Intent(this,MainActivity2::class.java)
             startActivity(intent)
 
-               signup.setOnClickListener {
+            signUp.setOnClickListener {
                    val intent = Intent(this,sign_up::class.java)
                    startActivity(intent)
                }
--- app/src/main/java/com/example/ah_lms/sign_up.kt
+++ app/src/main/java/com/example/ah_lms/sign_up.kt
@@ -3,6 +3,8 @@ package com.example.ah_lms
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_sign_up.*
 
@@ -12,7 +14,11 @@ class sign_up : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up)
 
-        btnsignUp.setOnClickListener {
+        val email:EditText = findViewById(R.id.emailEt)
+        val passwoed:EditText = findViewById(R.id.confirmPasswordEt)
+        val signup:Button = findViewById(R.id.registerBtn)
+
+        signup.setOnClickListener {
             val intent = Intent(this,MainActivity::class.java)
             startActivity(intent)
 
