0ecb0a5234b9f113151411d4a3814d6b9ad040fb
sakthi-kt <sakthiveljeevaa@gmail.com>
2022-09-12 16:32:27 +0530

    update

--- app/src/main/java/com/example/ah_lms/fragments/MyLeaveFragment.kt
+++ app/src/main/java/com/example/ah_lms/fragments/MyLeaveFragment.kt
@@ -5,17 +5,69 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.ah_lms.R
+import com.example.ah_lms.adapter.MyLeaveAdapter
+import com.example.ah_lms.dataclass.myLeave
 
 
 class MyLeaveFragment : Fragment() {
+    lateinit var newRecyclerView: RecyclerView
+    lateinit var newLeaveList: ArrayList<myLeave>
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?,
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_my_leave, container, false)
+    lateinit var profileImage:Array<Int>
+    lateinit var heading:Array<String>
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?, ): View? {
+
+        val view = inflater.inflate(R.layout.fragment_my_leave, container, false)
+        return view
+    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+        profileImage = arrayOf(
+            R.drawable.profile,
+            R.drawable.profile,
+            R.drawable.profile,
+            R.drawable.profile,
+            R.drawable.profile,
+            R.drawable.profile,
+            R.drawable.profile,
+            R.drawable.profile,
+            R.drawable.profile
+
+
+        )
+        heading = arrayOf(
+            "Sakthivel",
+            "Sakthivel",
+            "Sakthivel",
+            "Sakthivel",
+            "Sakthivel",
+            "Sakthivel",
+            "Sakthivel",
+            "Sakthivel",
+            "Sakthivel"
+        )
+
+
+        newRecyclerView = view.findViewById(R.id.leave_recyc)
+        newRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+        newRecyclerView.setHasFixedSize(true)
+        newLeaveList = arrayListOf<myLeave>()
+        getUserData()
+    }
+
+    private fun getUserData() {
+        for (i in profileImage.indices){
+
+            val leaves = myLeave(profileImage[i],heading[i])
+            newLeaveList.add(leaves)
+        }
+        newRecyclerView.adapter = MyLeaveAdapter(newLeaveList)
     }
 
 
--- app/src/main/java/com/example/ah_lms/register/RegisterActivity.kt
+++ app/src/main/java/com/example/ah_lms/register/RegisterActivity.kt
@@ -1,12 +1,24 @@
 package com.example.ah_lms.register
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.ah_lms.R
+import com.example.ah_lms.databinding.ActivityRegisterBinding
+import com.example.ah_lms.login.MainActivity
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+        login.setOnClickListener {
+            val nav = Intent(this@RegisterActivity,MainActivity::class.java)
+            startActivity(nav)
+        }
     }
 }
